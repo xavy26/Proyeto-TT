2f6881500f471d084cef876771ec702a7e826ff8
Jhon Carrión <jhon.carrion@unl.edu.ec>
2022-02-22 13:43:14 -0500

    wallet y defray (metodo de votar)

--- chaincode/evoting/evoting.go
+++ chaincode/evoting/evoting.go
@@ -22,18 +22,18 @@ type SmartContract struct {
 // Voter describes the basic data of a voter
 type Voter struct {
   ID string `json:"id" valid:"required"`
-  Name string `json:"name" valid:"required,alpha"`
-  Last_Name string `json:"last_name" valid:"required,alpha"`
+  Name string `json:"name" valid:"required"`
+  Last_Name string `json:"last_name" valid:"required"`
   Dni string `json:"dni" valid:"required,alphanum"`
   Email string `json:"email" valid:"email"`
-  Function string `json:"function" valid:"required,alpha"`
+  Function string `json:"function" valid:"required"`
 	Created_At time.Time `json:"created_at" valid:"required"`
 }
 
 // Candidate describes the basic data of a candidate
 type Candidate struct {
   ID string `json:"id" valid:"required"`
-  Position string `json:"position" valid:"required,alpha"`
+  Position string `json:"position" valid:"required"`
   Photo string `json:"photo" valid:"required,base64"`
   Voter Voter `json:"voter" valid:"required"`
 	Created_At time.Time `json:"created_at" valid:"required"`
@@ -42,7 +42,7 @@ type Candidate struct {
 // Political_Party describes the basic data of a political party
 type Political_Party struct {
   ID string `json:"id" valid:"required"`
-  Name string `json:"name" valid:"required,alpha"`
+  Name string `json:"name" valid:"required"`
   Description string `json:"description" valid:"required"`
   Logo string `json:"logo" valid:"required,base64"`
   Candidates []Candidate `json:"political_partys" valid:"required"`
@@ -52,7 +52,7 @@ type Political_Party struct {
 // Election describes the basic data of a election
 type Election struct {
   ID string `json:"id" valid:"required"`
-	Name string `json:"name" valid:"required,alpha"`
+	Name string `json:"name" valid:"required"`
   Description string `json:"description" valid:"required"`
   Date_Hour_Strat string `json:"date_hour_start" valid:"required"`
   Date_Hour_end string `json:"date_hour_end" valid:"required"`
@@ -84,6 +84,23 @@ type Item_Result struct {
 	Political_Party Political_Party `json:"political_party" valid:"required"`
   Nro_Votes int `json:"nro_votes" valid:"required,numeric"`
   Result Result `json:"result" valid:"required"`
+	Created_At time.Time `json:"created_at" valid:"required"`
+}
+
+// Wallet describes the data of the results of each wallets
+type Wallet struct {
+  ID string `json:"id" valid:"required"`
+	Owner Voter `json:"owner"`
+	Political_Party Political_Party `json:"owner"`
+	Created_At time.Time `json:"created_at" valid:"required"`
+}
+
+// Token_Wallet describes the data of the results of each tokens in wallets
+type Token_Wallet struct {
+  ID string `json:"id" valid:"required"`
+	Wallet Wallet `json:"wallet" valid:"required"`
+	Election Election `json:"election" valid:"required"`
+	Token_Amount int `json:"nro_votes" valid:"required,numeric"`
 	Created_At time.Time `json:"created_at" valid:"required"`
 }
 
@@ -1011,6 +1028,486 @@ func (s *SmartContract) Item_ResultHistory(ctx contractapi.TransactionContextInt
 	}
 
 	 return item_results, nil
+}
+
+/*
++++++++++++++++
++ WALLET +
++++++++++++++++
+*/
+
+func (s *SmartContract) CreateWallet(ctx contractapi.TransactionContextInterface, walletId string, token_amount int, owner Voter, political_party Political_Party, election Election) error {
+	// Validaciones de negocio
+	if owner == nil && political_party == nil {
+		return fmt.Errorf("Se debe enviar o un owner o un political party")
+	}
+	if owner != nil && political_party != nil {
+		return fmt.Errorf("Se debe enviar o un owner o un political party, pero no ambos")
+	}
+	exists, err := s.WalletExists(ctx, walletId)
+	if err != nil {
+		return err
+	}
+	if exists {
+		return fmt.Errorf("la Wallet %s ya existe", walletId)
+	}
+
+	exists, err := s.Token_WalletExists(ctx, token_walletId)
+	if err != nil {
+		return err
+	}
+	if exists {
+		return fmt.Errorf("El Token de la Wallet %s ya existe", walletId)
+	}
+	// Crear instancia de Wallet
+	wallet := Wallet{
+		ID: walletId,
+    Owner: owner,
+    Political_Party: political_party,
+		Created_At: time.Now(),
+	}
+
+	// Validaciones de sintaxis
+	valid, err := govalidator.ValidateStruct(wallet)
+	if err != nil {
+		fmt.Printf("Errores de validacion de campos: %s", err.Error())
+		return err
+	}
+  fmt.Printf("estado de validacion: %s", valid)
+	// comvertir Wallet en arreglo de bytes para enviar al ledger
+	walletAsBytes, err := json.Marshal(wallet)
+	if err != nil {
+		fmt.Printf("Marshall error: %s", err.Error())
+		return err
+	}
+	// gusrdar nueva wallet
+	err := ctx.GetStub().PutState(walletId, walletAsBytes)
+  if err != nil {
+    fmt.Printf("Error al crear la wallet: %s", err.Error())
+    return err
+  }
+  // otener la walet guardada
+  wallet_get, err := GetWalletById(ctx, walletId)
+  if err != nil {
+    fmt.Printf("Error al obtener la walet: %s", err.Error())
+    return err
+  }
+  // Crear instancia de Token Wallet
+  token_wallet := Token_Wallet{
+    ID: token_walletId,
+    Wallet: wallet_get,
+  	Election: election,
+  	Token_Amount: token_amount,
+    Created_At: time.Now(),
+  }
+
+  // Validaciones de sintaxis
+  valid, err := govalidator.ValidateStruct(token_wallet)
+  if err != nil {
+    fmt.Printf("Errores de validacion de campos: %s", err.Error())
+    return err
+  }
+  fmt.Printf("estado de validacion: %s", valid)
+  // comvertir Wallet en arreglo de bytes para enviar al ledger
+  token_walletAsBytes, err := json.Marshal(token_wallet)
+  if err != nil {
+    fmt.Printf("Marshall error: %s", err.Error())
+    return err
+  }
+  // gusrdar nueva wallet
+  return ctx.GetStub().PutState(token_walletId, token_walletAsBytes)
+}
+
+func (s *SmartContract) GetWalletById(ctx contractapi.TransactionContextInterface, walletId string) (*Wallet, error) {
+	walletAsBytes, err := ctx.GetStub().GetState(walletId)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to read from world state: %s", err.Error())
+	}
+	if walletAsBytes != nil {
+		return nil, fmt.Errorf("%s does no exist", walletId)
+	}
+
+	wallet := new(Wallet)
+	err = json.Unmarshal(walletAsBytes, wallet)
+	if err != nil {
+		return nil, fmt.Errorf("Unmarshal error: %s", err.Error())
+	}
+
+	return wallet, nil
+}
+
+func (s *SmartContract) GetWalletByOwner(ctx contractapi.TransactionContextInterface, owner Voter) (*Wallet, error) {
+// La consulta de rango con una cadena vacía para startKey y endKey realiza una consulta abierta de todos los activos en el espacio de nombres del código de cadena.
+ resultsIterator, err := ctx.GetStub().GetStateByRange("", "")
+ if err != nil {
+	 return nil, err
+ }
+// cerrar comunicación
+ defer resultsIterator.Close()
+// crear variable array para contener los registros
+ var wallets []*Wallet
+ // iterar mistras hayan registros que leer HasNext()
+ for resultsIterator.HasNext() {
+	 // obtener el siguiente reguiente
+	 queryResponse, err := resultsIterator.Next()
+	 if err != nil {
+		 return nil, err
+	 }
+	 // crear variable para instanciar Wallet
+	 wallet := new(Wallet)
+	 // convertir Bytes array en JSON y asignar a la variable wallet
+	 err = json.Unmarshal(queryResponse.Value, wallet)
+	 if err != nil {
+		 return nil, fmt.Errorf("Unmarshal error: %s", err.Error())
+	 }
+   if wallet.Owner == owner.ID {
+     wallets = append(wallets, wallet)
+   }
+ }
+ if len(wallets) == 1 {
+   return wallets[0], nil
+ }else{
+   if len(wallets) == 0 {
+     return nil, fmt.Errorf("El usuario no posee una wallet creada")
+   }else{
+     return nil, fmt.Errorf("El usuario posee más de una wallet creada")
+   }
+ }
+}
+
+func (s *SmartContract) GetWalletByPoliticalParty(ctx contractapi.TransactionContextInterface, political_party Political_Party) (*Wallet, error) {
+// La consulta de rango con una cadena vacía para startKey y endKey realiza una consulta abierta de todos los activos en el espacio de nombres del código de cadena.
+ resultsIterator, err := ctx.GetStub().GetStateByRange("", "")
+ if err != nil {
+	 return nil, err
+ }
+// cerrar comunicación
+ defer resultsIterator.Close()
+// crear variable array para contener los registros
+ var wallets []*Wallet
+ // iterar mistras hayan registros que leer HasNext()
+ for resultsIterator.HasNext() {
+	 // obtener el siguiente reguiente
+	 queryResponse, err := resultsIterator.Next()
+	 if err != nil {
+		 return nil, err
+	 }
+	 // crear variable para instanciar Wallet
+	 wallet := new(Wallet)
+	 // convertir Bytes array en JSON y asignar a la variable wallet
+	 err = json.Unmarshal(queryResponse.Value, wallet)
+	 if err != nil {
+		 return nil, fmt.Errorf("Unmarshal error: %s", err.Error())
+	 }
+   if wallet.Political_Party == political_party.ID {
+     wallets = append(wallets, wallet)
+   }
+ }
+ if len(wallets) == 1 {
+   return wallets[0], nil
+ }else{
+   if len(wallets) == 0 {
+     return nil, fmt.Errorf("El partido político no posee una wallet creada")
+   }else{
+     return nil, fmt.Errorf("El partido político posee más de una wallet creada")
+   }
+ }
+}
+
+func (s *SmartContract) GetTokensAvailable(ctx contractapi.TransactionContextInterface, owner Voter) ([]*Token_Wallet, error) {
+// obtener a wallet a la que pertenecen los tokens
+wallet, err := GetWalletByOwner(ctx, owner)
+if err != nil {
+  fmt.Errorf("No se pudo obtener la wallet: %s", err.Error())
+  return nil, err
+}
+// La consulta de rango con una cadena vacía para startKey y endKey realiza una consulta abierta de todos los activos en el espacio de nombres del código de cadena.
+ resultsIterator, err := ctx.GetStub().GetStateByRange("", "")
+ if err != nil {
+	 return nil, err
+ }
+// cerrar comunicación
+ defer resultsIterator.Close()
+// crear variable array para contener los registros
+ var tokens []*Token_Wallet
+ // iterar mistras hayan registros que leer HasNext()
+ for resultsIterator.HasNext() {
+	 // obtener el siguiente reguiente
+	 queryResponse, err := resultsIterator.Next()
+	 if err != nil {
+		 return nil, err
+	 }
+	 // crear variable para instanciar Wallet
+	 token := new(Token_Wallet)
+	 // convertir Bytes array en JSON y asignar a la variable wallet
+	 err = json.Unmarshal(queryResponse.Value, token)
+	 if err != nil {
+		 return nil, fmt.Errorf("Unmarshal error: %s", err.Error())
+	 }
+   if token.Wallet == wallet.ID {
+     tokens = append(tokens, token)
+   }
+ }
+ return tokens, nil
+}
+
+func (s *SmartContract) GetTokenByPoliticalPartyForElection(ctx contractapi.TransactionContextInterface, political_party Political_Party, election Election) (*Token_Wallet, error) {
+// obtener a wallet a la que pertenecen los tokens
+wallet, err := GetWalletByPoliticalParty(ctx, political_party)
+if err != nil {
+  fmt.Errorf("No se pudo obtener la wallet: %s", err.Error())
+  return nil, err
+}
+// La consulta de rango con una cadena vacía para startKey y endKey realiza una consulta abierta de todos los activos en el espacio de nombres del código de cadena.
+ resultsIterator, err := ctx.GetStub().GetStateByRange("", "")
+ if err != nil {
+	 return nil, err
+ }
+// cerrar comunicación
+ defer resultsIterator.Close()
+// crear variable array para contener los registros
+ var tokens []*Token_Wallet
+ // iterar mistras hayan registros que leer HasNext()
+ for resultsIterator.HasNext() {
+	 // obtener el siguiente reguiente
+	 queryResponse, err := resultsIterator.Next()
+	 if err != nil {
+		 return nil, err
+	 }
+	 // crear variable para instanciar Wallet
+	 token := new(Token_Wallet)
+	 // convertir Bytes array en JSON y asignar a la variable wallet
+	 err = json.Unmarshal(queryResponse.Value, token)
+	 if err != nil {
+		 return nil, fmt.Errorf("Unmarshal error: %s", err.Error())
+	 }
+   if token.Wallet == wallet.ID && token.election == election.ID {
+     tokens = append(tokens, token)
+   }
+ }
+ if len(tokens) == 1 {
+	 return tokens[0], nil
+ } else {
+	 if len(tokens) == 0 {
+		 return nil, fmt.Errorf("El partido político no posee tokens disponibles para usar en la elección")
+	 } else {
+		 return nil, fmt.Errorf("El partido político tiene registrados mas de un token en la elección")
+	 }
+ }
+}
+
+func (s *SmartContract) HasTokensAvailableForElection(ctx contractapi.TransactionContextInterface, owner Voter, election Election) (*Token_Wallet, bool, error) {
+// La consulta de rango con una cadena vacía para startKey y endKey realiza una consulta abierta de todos los activos en el espacio de nombres del código de cadena.
+ resultsIterator, err := ctx.GetStub().GetStateByRange("", "")
+ if err != nil {
+	 return nil, false, err
+ }
+// cerrar comunicación
+ defer resultsIterator.Close()
+// crear variable array para contener los registros
+ var tokens []*Token_Wallet
+ // iterar mistras hayan registros que leer HasNext()
+ for resultsIterator.HasNext() {
+	 // obtener el siguiente reguiente
+	 queryResponse, err := resultsIterator.Next()
+	 if err != nil {
+		 return nil, false, err
+	 }
+	 // crear variable para instanciar Wallet
+	 token := new(Token_Wallet)
+	 // convertir Bytes array en JSON y asignar a la variable wallet
+	 err = json.Unmarshal(queryResponse.Value, token)
+	 if err != nil {
+		 return nil, false, fmt.Errorf("Unmarshal error: %s", err.Error())
+	 }
+   if token.Token_Amount > 0 && election.ID == token.election {
+     tokens = append(tokens, token)
+   }
+ }
+ if len(tokens) == 1 {
+   return tokens[0], true, nil
+ } else {
+   if len(tokens) == 0 {
+     return nil, false, fmt.Errorf("El usuario ya no posee tokens disponibles para usar en la elección")
+   } else {
+     return nil, false, fmt.Errorf("El usuario tiene registrados mas de un token en la elección")
+   }
+ }
+}
+
+func (s *SmartContract) WalletExists(ctx contractapi.TransactionContextInterface, walletId string) (bool, error) {
+	walletJSON, err := ctx.GetStub().GetState(walletId)
+	if err != nil {
+		return false, fmt.Errorf("failed to read from world state: %v", err)
+	}
+
+	return walletJSON != nil, nil
+}
+
+func (s *SmartContract) Token_WalletExists(ctx contractapi.TransactionContextInterface, token_walletId string) (bool, error) {
+	token_walletJSON, err := ctx.GetStub().GetState(token_walletId)
+	if err != nil {
+		return false, fmt.Errorf("failed to read from world state: %v", err)
+	}
+
+	return token_walletJSON != nil, nil
+}
+
+func (s *SmartContract) WalletHistory(ctx contractapi.TransactionContextInterface, walletId string) ([]*Wallet, error) {
+	walletsIterator, err := ctx.GetStub().GetHistoryForKey(walletId)
+
+	if err != nil {
+		return nil, fmt.Errorf("Failed to get from history: %s", err.Error())
+	}
+	if walletsIterator != nil {
+		return nil, fmt.Errorf("%s does no exist", walletId)
+	}
+
+	defer walletsIterator.Close()
+
+	var wallets []*Wallet
+
+	for walletsIterator.HasNext() {
+		queryResponse, err := walletsIterator.Next()
+		if err != nil {
+			fmt.Println(err.Error())
+			return nil, err
+		}
+
+		wallet := new(Wallet)
+
+		err = json.Unmarshal(queryResponse.Value, wallet)
+		if err != nil {
+			fmt.Println(err.Error())
+			return nil, err
+		}
+		wallets = append(wallets, wallet)
+	}
+
+	 return wallets, nil
+}
+
+func (s *SmartContract) Token_WalletHistory(ctx contractapi.TransactionContextInterface, token_walletId string) ([]*Token_Wallet, error) {
+	token_walletsIterator, err := ctx.GetStub().GetHistoryForKey(token_walletId)
+
+	if err != nil {
+		return nil, fmt.Errorf("Failed to get from history: %s", err.Error())
+	}
+	if token_walletsIterator != nil {
+		return nil, fmt.Errorf("%s does no exist", token_walletId)
+	}
+
+	defer token_walletsIterator.Close()
+
+	var token_wallets []*Token_Wallet
+
+	for token_walletsIterator.HasNext() {
+		queryResponse, err := token_walletsIterator.Next()
+		if err != nil {
+			fmt.Println(err.Error())
+			return nil, err
+		}
+
+		token_wallet := new(Token_Wallet)
+
+		err = json.Unmarshal(queryResponse.Value, token_wallet)
+		if err != nil {
+			fmt.Println(err.Error())
+			return nil, err
+		}
+		token_wallets = append(token_wallets, token_wallet)
+	}
+
+	 return token_wallets, nil
+}
+/*
+* METODO DE REGISTRO DE VOTO DESDE UN VOTANTE HACIA UN PARTIDO POLITICO
+*/
+func (s *SmartContract) Defray (ctx contractapi.TransactionContextInterface, ownerId string, political_partyId string, electionId string) (bool, error) {
+	election, err := GetElectionById(ctx, electionId)
+	if err != nil {
+		return false, fmt.Errorf("Error al obtener la Elección: %s", err.Error())
+	}
+	voter, err := GetVoterById(ctx, ownerId)
+	if err != nil {
+		return false, fmt.Errorf("Error al obtener el votante: %s", err.Error())
+	}
+	wallet_owner, err := GetWalletByOwner(ctx, voter)
+	if err != nil {
+		return false, fmt.Errorf("Error al obtener la wallet del votante: %s", err.Error())
+	}
+	political_party, err := GetPoliticalPartyById(ctx, political_partyId)
+	if err != nil {
+		return false, fmt.Errorf("Error al obtener el partido politico: %s", err.Error())
+	}
+	wallet_pp, err := GetWalletByPoliticalParty(ctx, political_party)
+	if err != nil {
+		return false, fmt.Errorf("Error al obtener la wallet del partido politico: %s", err.Error())
+	}
+
+	token, flag, err := HasTokensAvailableForElection(ctx, voter, election)
+
+	if flag {
+		token_pp, err := GetTokenByPoliticalPartyForElection(ctx, political_party, election)
+		if err != nil {
+			return false, fmt.Errorf("Error al obtener el token del partido politico: %s", err.Error())
+		}
+		token_own := Token_Wallet{
+	    ID: token.ID,
+	    Wallet: token.Wallet,
+	  	Election: token.Election,
+	  	Token_Amount: token.Token_Amount -1,
+	    Created_At: token.Created_At,
+	  }
+
+		// Validaciones de sintaxis
+		valid, err := govalidator.ValidateStruct(token_own)
+		if err != nil {
+			fmt.Printf("Errores de validacion de campos: %s", err.Error())
+			return false, err
+		}
+		fmt.Printf("estado de validacion: %s", valid)
+		// comvertir Wallet en arreglo de bytes para enviar al ledger
+		token_walletAsBytes, err := json.Marshal(token_wallet)
+		if err != nil {
+			fmt.Printf("Marshall error: %s", err.Error())
+			return false, err
+		}
+		// gusrdar nueva wallet
+		err := ctx.GetStub().PutState(token.ID, token_walletAsBytes)
+		if err != nil {
+			return false, fmt.Errorf("Error al obtener la wallet del partido politico: %s", err.Error())
+		}
+
+		token_po_pa := Token_Wallet{
+	    ID: token_pp.ID,
+	    Wallet: token_pp.Wallet,
+	  	Election: token_pp.Election,
+	  	Token_Amount: token_pp.Token_Amount +1,
+	    Created_At: token_pp.Created_At,
+	  }
+		// Validaciones de sintaxis
+		valid, err := govalidator.ValidateStruct(token_po_pa)
+		if err != nil {
+			fmt.Printf("Errores de validacion de campos: %s", err.Error())
+			return false, err
+		}
+		fmt.Printf("estado de validacion: %s", valid)
+		// comvertir Wallet en arreglo de bytes para enviar al ledger
+		token_walletAsBytes, err := json.Marshal(token_wallet)
+		if err != nil {
+			fmt.Printf("Marshall error: %s", err.Error())
+			return false, err
+		}
+		// gusrdar nueva wallet
+		err := ctx.GetStub().PutState(token_pp.ID, token_walletAsBytes)
+		if err != nil {
+			return false, fmt.Errorf("Error al obtener la wallet del partido politico: %s", err.Error())
+		}
+		return true, nil
+	} else {
+		return false, fmt.Errorf("Problema al saber si el votante puede votar: %s", err.Error())
+	}
 }
 
 /*******
